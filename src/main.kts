b643c3cfac41fc7485d0a9b6eff2bf4c885d57da
Torres <dan03torres@gmail.com>
2022-05-18 19:39:08 -0300

    Test

--- src/main.kts
+++ src/main.kts
@@ -1,1 +1,9 @@
-print("Teste")
+val accountOwner: String = "Daniel"
+val accountNumber: Int = 1000
+var accountBalance: Double = 0.0
+
+saldo += 100
+
+println("Welcome to TechBank $accountOwner!\nYour account number is $accountNumber\nYour account balance is: $accountBalance")
+
+
