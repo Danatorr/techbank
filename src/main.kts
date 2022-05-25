33dc1306f3125e09295f97ef801d84ce534dccf4
Torres <dan03torres@gmail.com>
2022-05-24 21:09:25 -0300

    OOP Started

--- src/main.kts
+++ src/main.kts
@@ -1,27 +1,42 @@
-for (i in 1..5) {
-    val accountOwner: String = "Daniel $i"
-    val accountNumber: Int = 1000 + i
-    var accountBalance: Double = 0.0 + i
-
-    accountBalance += 100
-
-    println("Welcome to TechBank $accountOwner!\nYour account number is $accountNumber\nYour account balance is: $accountBalance")
-    checkBalance(accountBalance)
-    //This is similar to switch case and replaces the code below it
-    //when {
-    //    accountBalance > 0 -> {
-    //        println("Your account balance is postive!")
-    //    }
-    //    accountBalance == 0 -> {
-    //        println("Your account balance is empty!")
-    //    }
-    //    else -> {
-    //        println("Your account balance is negative!")
-    //    }
+//for (i in 1..5) {
+//    val accountOwner: String = "Daniel $i"
+//    val accountNumber: Int = 1000 + i
+//    var accountBalance: Double = 0.0 + i
+//
+//    accountBalance += 100
+//
+//
 //}
+
+val account01 = Account()
+val account02 = Account()
+
+account01.accountOwner = "Daniel"
+account01.accountNumber = 1000
+account01.accountBalance = 100.0
+
+account02.accountOwner = "Alice"
+account02.accountNumber = 1001
+account02.accountBalance = 500.0
+
+
+
+println(account01.accountOwner)
+println(account01.accountNumber)
+println(account01.accountBalance)
+
+println(account02.accountOwner)
+println(account02.accountNumber)
+println(account02.accountBalance)
+
+//println("Welcome to TechBank $accountOwner!\nYour account number is $accountNumber\nYour account balance is: $accountBalance")
+
+class Account(){
+    var accountOwner: String = ""
+    var accountNumber: Int = 0
+    var accountBalance: Double = 0.0
+
 }
-
-
 
 fun checkBalance(accountBalance: Double) {
     if (accountBalance > 0) {
